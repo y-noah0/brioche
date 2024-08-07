cd1aeef60a06148142d1650e2de57f0b7db41269
Yannick Noah <noah55009@gmail.com>
2024-08-07 10:53:55 +0200

    added galery section

--- brioche/src/components/Home/Home.jsx
+++ brioche/src/components/Home/Home.jsx
@@ -3,7 +3,9 @@ import Navigation from "./Nav/Navigation";
 import Drinks from "./Drinks/drinks";
 import Header from "./Header/Header";
 import Description from "./Description/Description";
- import Footer from "../Footer/Footer";
+import Footer from "../Footer/Footer";
+import Gallery from "./Gallery/Gallery";
+
 
 
 function Home(){
@@ -14,6 +16,7 @@ function Home(){
             <Description />
             <Drinks />
             <About />
+            <Gallery/>
             <Footer />
 
         </>
