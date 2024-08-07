f8b2117afcd378bbbb97d34583ac5534a5d43891
Yannick Noah <noah55009@gmail.com>
2024-08-07 11:00:12 +0200

    added analytics

--- brioche/src/App.jsx
+++ brioche/src/App.jsx
@@ -1,8 +1,9 @@
-
+import { Analytics } from "@vercel/analytics/react"
 import AppRouter from "./components/BrowserRoutes";
 function App() {
   return (
     <>
+    <Analytics/>
     <AppRouter />
     </>
   )
