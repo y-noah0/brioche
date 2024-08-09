aa9d7d8ee7d060ef504178539dfd5d4e246fa640
Yannick Noah <noah55009@gmail.com>
2024-08-09 18:20:18 +0200

    finished the gallery section

--- Frontend/brioche/src/components/Home/Gallery/Gallery.jsx
+++ Frontend/brioche/src/components/Home/Gallery/Gallery.jsx
@@ -1,13 +1,15 @@
+// import React from 'react';
 import gallery_images from './gallery_images';
 import style from './style.module.css';
 
-function Gallery(){
+function Gallery() {
     return (
         <div className={style.Gallery}>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
             <div className={style.text_container}>
                 <h4>Instagram</h4>
                 <h2>Photo Gallery</h2>
-                <p>Discover our bakerys latest photography</p>
+                <p>Discover our bakery's latest photography</p>
                 <button className={style.button}>Discover More</button>
             </div>
             <div className={style.image_container}>
@@ -15,8 +17,12 @@ function Gallery(){
                 <img src={gallery_images[2]} alt="Brioche_gallery" />
                 <img src={gallery_images[3]} alt="Brioche_gallery" />
                 <img src={gallery_images[0]} alt="Brioche_gallery" />
+                <div className={style.arrow_container}>
+                    <button className={style.more_button}><i className='fas fa-arrow-circle-right' style={{ fontSize: '48px', color: 'RGBA(222,222,222,0.1)' }}></i></button>
+                </div>
             </div>
         </div>
     )
 }
+
 export default Gallery;
