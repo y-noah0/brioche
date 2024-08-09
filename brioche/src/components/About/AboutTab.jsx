a35783432835915322f8ec570e745ea5d2a6261d
Yannick Noah <noah55009@gmail.com>
2024-08-09 12:00:43 +0200

    added the frontend folder

--- brioche/src/components/About/AboutTab.jsx
+++
@@ -1,8 +0,0 @@
-import Navigation from "../Home/Nav/Navigation";
-function AboutTab(){
-    return(
-        <Navigation/>
-        
-    )
-}
-export default AboutTab;
--- brioche/src/components/Menu/Menu.jsx
+++
@@ -1,8 +0,0 @@
-function Menu() {
-    return(
-        <>
-        <h2>Our menu is a symphony of flavors</h2>
-        </>
-    )
-}
-export default Menu;
